1de41eb38929bd08287c6ce3c908362d1b9a8170
divya1viswanath <divyarugmini798@gmail.com>
2022-05-25 23:11:07 +0530

    searh student

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Addevent from './component/Addevent';
+import Searchstudent from './component/Searchstudent';
 
 function App() {
   return (
     <>
       <Addevent/>
+      <Searchstudent/>
     </>
   );
 }
